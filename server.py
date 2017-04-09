11ae1266b4559fa6fdaaa032802d349898068484
Bakhtiyar Syed <baksoverflow@gmail.com>
2017-04-09 11:28:38 +0530

    Added subnet_to_ip function and fixed a few cases where extra mac addresses might result in subnets.conf file

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import re
+import math
 from operator import itemgetter
 
 
@@ -13,7 +14,7 @@ def validate_CIDR(CIDR_format_string):
     subnet_mask = CIDR_format_string[1]
 
     if not re.match(valid_IP_regex, ip):
-        print "Invalid IPv4 has been provided."
+        print ("Invalid IPv4 has been provided.")
 
     try:
         subnet_mask = int(subnet_mask)
@@ -22,8 +23,35 @@ def validate_CIDR(CIDR_format_string):
         sys.exit(1)
 
     if not subnet_mask < 33:
-        print "Invalid Subnet Mask has been provided."
+        print ("Invalid Subnet Mask has been provided.")
+        sys.exit(1)
 
+
+def convert_mask_to_ip(subnet_mask):
+    # ex. 24 -> 255.255.255.0
+    subnet_list = []
+    for x in xrange(4): # creating list of four 0s
+        subnet_list.append(0)
+    
+    #print (subnet_list)
+    try:
+        octets = int(subnet_mask / 8)  # how many octets of 255
+    except TypeError as err:
+        print ("Type Error for your Subnet Mask provided: {0}".format(err))
+        sys.exit(1)
+
+    if (octets <= 0):
+        rem_subnet = 8 - subnet_mask
+        #Fixed an int function
+        subnet_list[0] = int(256 - pow(2,rem_subnet))
+
+    else:
+        for i in xrange(octets):
+            subnet_list[i] = 255
+        rem_subnet = 8 - (subnet_mask - 8 * octets)
+        subnet_list[i+1] = int(256 - pow(2,rem_subnet))
+    
+    return subnet_list
 
 def main():
 
@@ -35,10 +63,15 @@ def main():
         sys.exit(1)
 
     file_content = subnet_file.readlines()
+    #print file_content
     file_content = [x.strip() for x in file_content]
+    #print (file_content)
 
     # Validate the CIDR formatted: [IPv4]/[SubnetMask]
     validate_CIDR(file_content[0])
+
+    # Store the validated CIDR in a variable for future use.
+    CIDR = file_content[0]
     
     # Validate the type of "number of labs"
     try:
@@ -57,23 +90,63 @@ def main():
         this_line = file_content[i].split(':')
         labs_dict.update({str(this_line[0]): [int(this_line[1])]})
 
-    for i in range(2+num_of_labs, 2+num_of_labs*2):
+    #print (labs_dict)
+
+    for i in range(2+num_of_labs, 2+num_of_labs*3):
         this_line = file_content[i].split('-')
         labs_dict[this_line[1]].append(str(this_line[0]))
+
+    print (labs_dict)
 
     for key, value in labs_dict.items():
         labs.append(key)
         capacity_of_labs.append(value[0])
-        mac_add.append(value[1])
+        #We may have multiple MAc addresses, so adding that to the mac_address list
+        for i in value[1:]:
+            mac_add.append(str(i))
 
-    labs_info = zip(labs, capacity_of_labs, mac_add)
+    print (mac_add)
+
+    #No need to add mac address to labs(as of now atleast!)
+    labs_info = zip(labs, capacity_of_labs)
+
+    #print (labs_info)
 
     # Sort labs according to number of hosts - ('Lab_name', number_of_hosts, 'MAC addr')
-    labs_info = sorted(labs_info, key=itemgetter(1))
+    #Added reverse = True
+    labs_info = sorted(labs_info, key=itemgetter(1), reverse=True)
 
     # Print them one by one
     for eachLab in labs_info:
         print eachLab
+
+
+
+    #VLSM SUBNET MASKING AND ASSIGNING IP ADDRESSES 
+
+    #Split the subnet CIDR_format_string
+    CIDR_format_string = CIDR.split('/')
+    ip = CIDR_format_string[0]
+    subnet_mask = CIDR_format_string[1]
+    print (subnet_mask)
+
+    #WE have to convert subnet masks to an equivalent IP format for processing. 
+
+    subnet_list = convert_mask_to_ip(int(subnet_mask))
+    print subnet_list
+
+
+    
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':  # pragma: no cover
