2995597b67d7ab4fbf718a4b941a2fba05780b28
Bakhtiyar Syed <baksoverflow@gmail.com>
2017-04-13 18:11:07 +0530

    Added broadcast

--- client.py
+++ client.py
@@ -1,18 +1,20 @@
 import socket                   
 
-s = socket.socket()             
-host = ""
+#s = socket.socket()             
+#host = ""
 port = 1452
-
-s.connect((host, port))
-s.send("F8:D1:90:80:65:A8")
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+my_socket.sendto("F8:D1:90:80:65:A8", ('<broadcast>' ,port))
+#s.connect((host, port))
+#s.send("F8:D1:90:80:65:A8")
 
 while True:
     print('receiving data...')
-    data = s.recv(1024)
+    data = my_socket.recv(1024)
     print('data=%s', (data))
     if not data:
         break
 
-s.close()
+my_socket.close()
 print('connection closed')
